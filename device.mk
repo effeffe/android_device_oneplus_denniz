cd751efbcae5cd23e1cbe3be5f7718160b4eb6e8
TheMalachite <eliasgheeraert@gmail.com>
2021-09-09 20:20:46 +0200

    denniz: Initial skeleton tree
    Change-Id: Ieee563952f7b443c2a770353a7036163421f5669

---
+++ device.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.recovery.mt6893.rc \
+    fstab.mt6893_ramdisk
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# VNDK
+PRODUCT_SHIPPING_API_LEVEL := 30
